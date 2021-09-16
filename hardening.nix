af9e0f7d85e3c9c2e03bdf24b00bc82f77a8544c
Ryan Horiguchi <ryan.horiguchi@mimacom.com>
2021-09-16 17:45:06 +0200

    Update TODOs

--- hardening.nix
+++ hardening.nix
@@ -3,7 +3,11 @@
 
   security = {
     # TODO figure out how it works now
+    # https://github.com/NixOS/nixpkgs/blob/nixos-21.05/nixos/modules/security/apparmor.nix#L20
     # apparmor.profiles = [ "${pkgs.apparmor-profiles}/share/apparmor/extra-profiles" ];
+
+    # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/nixos/modules/security/apparmor.nix#L44
+    # https://github.com/NixOS/nixpkgs/blob/nixos-21.05/nixos/modules/security/apparmor/profiles.nix
     # apparmor.policies.profile = builtins.concatStringsSep "\n" [ "${pkgs.apparmor-profiles}/share/apparmor/extra-profiles" ];
 
     lockKernelModules = false;
--- overlays/citrix_workspace/default.nix
+++ overlays/citrix_workspace/default.nix
@@ -1,5 +1,4 @@
 { citrix_workspace, lib }:
 citrix_workspace.overrideAttrs (oldAttrs: {
-  # TODO check with derivation hash
-  src = assert lib.versions.splitVersion oldAttrs.version == [ "21" "6" "0" "28" ]; ./citrix_workspace_21.6.0.28.tar.gz;
+  src = assert oldAttrs.version == [ "21" "6" "0" "28" ]; ./citrix_workspace_21.6.0.28.tar.gz;
 })
