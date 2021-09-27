1dfffd0211c21f9f637e6a4e56cdb474b8323e45
Ryan Horiguchi <ryan.horiguchi@mimacom.com>
2021-09-27 18:04:35 +0200

    Add TODO

--- overlays/citrix_workspace/default.nix
+++ overlays/citrix_workspace/default.nix
@@ -1,2 +1,5 @@
 { citrix_workspace, lib }:
-citrix_workspace.overrideAttrs (oldAttrs: { src = assert oldAttrs.version == "21.6.0.28"; ./citrix_workspace_21.6.0.28.tar.gz; })
+citrix_workspace.overrideAttrs (oldAttrs: {
+  # TODO check with derivation hash
+  src = assert oldAttrs.version == "21.6.0.28"; ./citrix_workspace_21.6.0.28.tar.gz;
+})
