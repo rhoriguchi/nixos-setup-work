005949246fde5eb2097c77c8604d733aa769d01e
Ryan Horiguchi <ryan.horiguchi@mimacom.com>
2021-09-27 18:03:39 +0200

    Fix citrix_workspace overlay assertion

--- overlays/citrix_workspace/default.nix
+++ overlays/citrix_workspace/default.nix
@@ -1,2 +1,2 @@
 { citrix_workspace, lib }:
-citrix_workspace.overrideAttrs (oldAttrs: { src = assert oldAttrs.version == [ "21" "6" "0" "28" ]; ./citrix_workspace_21.6.0.28.tar.gz; })
+citrix_workspace.overrideAttrs (oldAttrs: { src = assert oldAttrs.version == "21.6.0.28"; ./citrix_workspace_21.6.0.28.tar.gz; })
