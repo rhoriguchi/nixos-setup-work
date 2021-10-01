044af4254a03371108e2d74b2b64770e77bd933f
Ryan Horiguchi <ryan.horiguchi@mimacom.com>
2021-10-01 11:15:35 +0200

    Add some more app armor possibilities

--- hardening.nix
+++ hardening.nix
@@ -1,14 +1,42 @@
-{ pkgs, ... }: {
+{ pkgs, lib, ... }: {
   imports = [ <nixpkgs/nixos/modules/profiles/hardened.nix> ];
 
   security = {
-    # TODO figure out how it works now
-    # https://github.com/NixOS/nixpkgs/blob/nixos-21.05/nixos/modules/security/apparmor.nix#L20
-    # apparmor.profiles = [ "${pkgs.apparmor-profiles}/share/apparmor/extra-profiles" ];
+    # apparmor.policies."extra-profiles" = let
+    #   excludePolicies = [ "README" ];
 
+    #   dir = "${pkgs.apparmor-profiles}/share/apparmor/extra-profiles";
+    #   policies = lib.filterAttrs (key: _: !(builtins.elem key excludePolicies)) (builtins.readDir dir);
+    # in {
+    #   enable = true;
+    #   profile = builtins.concatStringsSep "\n" (lib.mapAttrsToList (key: _: ''include "${dir}/${key}"'') policies);
+    # };
+
+    # apparmor.policies = let
+    #   excludePolicies = [ "README" ];
+
+    #   dir = "${pkgs.apparmor-profiles}/share/apparmor/extra-profiles";
+    #   policies = lib.filterAttrs (key: _: !(builtins.elem key excludePolicies)) (builtins.readDir dir);
+    # in lib.mapAttrs (key: _: {
+    #   enable = true;
+
+    #   profile = ''
+    #     include "${pkgs.apparmor-profiles}/etc/apparmor.d"
+    #     include "${dir}/${key}"
+    #   '';
+    # }) policies;
+
+    # TODO figure out how it works now
+    # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/nixos/modules/security/apparmor.nix#L20
     # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/nixos/modules/security/apparmor.nix#L44
-    # https://github.com/NixOS/nixpkgs/blob/nixos-21.05/nixos/modules/security/apparmor/profiles.nix
-    # apparmor.policies.profile = builtins.concatStringsSep "\n" [ "${pkgs.apparmor-profiles}/share/apparmor/extra-profiles" ];
+    # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/nixos/modules/security/apparmor/profiles.nix
+
+    # apparmor.policies."bin.ping".profile = lib.mkIf apparmor.policies."bin.ping".enable ''
+    #   include "${pkgs.iputils.apparmor}/bin.ping"
+    #   include "${pkgs.inetutils.apparmor}/bin.ping"
+    #   # Note that including those two profiles in the same profile
+    #   # would not work if the second one were to re-include <tunables/global>.
+    # '';
 
     lockKernelModules = false;
   };
