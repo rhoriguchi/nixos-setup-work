27e7cc31bda3286ee0a2289d6e742212c06e30a3
Ryan Horiguchi <ryan.horiguchi@mimacom.com>
2021-10-04 10:40:14 +0200

    Simplify autostart

--- rhoriguchi/autostart.nix
+++ rhoriguchi/autostart.nix
@@ -1,17 +1,8 @@
 { pkgs, lib, config, ... }:
-let packages = lib.unique (map (package: "${package.name}") (config.environment.systemPackages ++ config.users.users.rhoriguchi.packages));
+let packageNames = lib.unique (map (package: package.name) (config.environment.systemPackages ++ config.users.users.rhoriguchi.packages));
 in {
   imports = [ (import "${(import ../rhoriguchi-nixos-setup.nix).path}/configuration/rhoriguchi/autostart.nix") ];
 
-  home-manager.users.rhoriguchi.xdg.configFile."autostart/teams.desktop" = lib.mkIf (builtins.elem pkgs.teams.name packages) {
-    text = ''
-      [Desktop Entry]
-      Name=Microsoft Teams
-      Icon=teams
-      Exec=${pkgs.teams}/bin/teams %U
-      Terminal=false
-      Type=Application
-      X-GNOME-Autostart-enabled=true
-    '';
-  };
+  home-manager.users.rhoriguchi.xdg.configFile."autostart/${pkgs.teams.pname}.desktop" =
+    lib.mkIf (lib.elem pkgs.teams.name packageNames) { source = "${pkgs.teams}/share/applications/teams.desktop"; };
 }
