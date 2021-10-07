2f6784af8044f1b3720f367a00a135743ed8c6cb
Ryan Horiguchi <ryan.horiguchi@mimacom.com>
2021-10-07 11:47:59 +0200

    Update citrix tar

--- overlays/citrix_workspace/default.nix
+++ overlays/citrix_workspace/default.nix
@@ -1,5 +1,5 @@
 { citrix_workspace, lib }:
 citrix_workspace.overrideAttrs (oldAttrs: {
-  # TODO check with derivation hash
-  src = assert oldAttrs.version == "21.6.0.28"; ./citrix_workspace_21.6.0.28.tar.gz;
+  # TODO client version is actually 21.9.0.25
+  src = assert oldAttrs.version == "21.8.0.40"; ./citrix_workspace_21.9.0.25.tar.gz;
 })
