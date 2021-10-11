fd314e95a2647caacc554bbe2f15efa214569d9b
Ryan Horiguchi <ryan.horiguchi@mimacom.com>
2021-10-11 09:16:34 +0200

    Upgrade citrix_workspace version

--- overlays/citrix_workspace/default.nix
+++ overlays/citrix_workspace/default.nix
@@ -1,5 +1,4 @@
 { citrix_workspace, lib }:
 citrix_workspace.overrideAttrs (oldAttrs: {
-  # TODO client version is actually 21.9.0.25
-  src = assert oldAttrs.version == "21.8.0.40"; ./citrix_workspace_21.9.0.25.tar.gz;
+  src = assert oldAttrs.version == "21.9.0.25"; ./citrix_workspace_21.9.0.25.tar.gz;
 })
