c890cbf5f7d9a6d87438303994ed8059c3b4af22
Ryan Horiguchi <ryan.horiguchi@mimacom.com>
2021-10-21 13:17:51 +0200

    Format

--- overlays/citrix_workspace/default.nix
+++ overlays/citrix_workspace/default.nix
@@ -1,4 +1,2 @@
 { citrix_workspace, lib }:
-citrix_workspace.overrideAttrs (oldAttrs: {
-  src = assert oldAttrs.version == "21.9.0.25"; ./citrix_workspace_21.9.0.25.tar.gz;
-})
+citrix_workspace.overrideAttrs (oldAttrs: { src = assert oldAttrs.version == "21.9.0.25"; ./citrix_workspace_21.9.0.25.tar.gz; })
--- rhoriguchi/maven/default.nix
+++ rhoriguchi/maven/default.nix
@@ -1,6 +1,5 @@
 { lib, ... }: {
-  home-manager.users.rhoriguchi.home.file.".m2/settings.xml".text = lib.replaceStrings [ "USERNAME" "CREDENTIAL" ] [
-    "ryan.horiguchi@mimacom.com"
-    (import ../../secrets.nix).maven.credential
-  ] (builtins.readFile ./settings.xml);
+  home-manager.users.rhoriguchi.home.file.".m2/settings.xml".text =
+    lib.replaceStrings [ "USERNAME" "CREDENTIAL" ] [ "ryan.horiguchi@mimacom.com" (import ../../secrets.nix).maven.credential ]
+    (builtins.readFile ./settings.xml);
 }
