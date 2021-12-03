9fd43e763b0e648fe19230f37ed6e67fd095d730
Ryan Horiguchi <ryan.horiguchi@mimacom.com>
2021-12-03 09:51:39 +0100

    Update openconnect-sso

--- overlays/default.nix
+++ overlays/default.nix
@@ -4,9 +4,8 @@
         super.fetchFromGitHub {
           owner = "vlaci";
           repo = "openconnect-sso";
-          # TODO pin version once issue https://github.com/vlaci/openconnect-sso/issues/55 is fixed
-          rev = "54da0073732cc1cb445360c3d6f0c915ae223ec8";
-          sha256 = "1bx93rj4bncmhm6vyiabxrv76s028zszwxdxncnx2q8zhc0wqqyr";
+          rev = "v0.8.0";
+          sha256 = "sha256-adzqEWpsb4ITVPnQIns9yv71V83msYqEIUH0DjsmQVA=";
         }
       }/nix" { pkgs = super; }).openconnect-sso;
   })
