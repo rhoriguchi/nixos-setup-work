6cd30b892d2afc3626d9dd154a66934077095ba5
Ryan Horiguchi <ryan.horiguchi@mimacom.com>
2022-02-01 14:57:07 +0100

    Simplify

--- rhoriguchi/vscode.nix
+++ rhoriguchi/vscode.nix
@@ -1,10 +1,8 @@
 { pkgs, config, lib, ... }: {
   imports = [ (import "${(import ../rhoriguchi-nixos-setup.nix).path}/configuration/rhoriguchi/vscode.nix") ];
 
-  home-manager.users.rhoriguchi.programs.vscode.userSettings = {
-    "gitlens.remotes" = [{
-      "type" = "GitLab";
-      "domain" = "code.flowable.com";
-    }];
-  };
+  home-manager.users.rhoriguchi.programs.vscode.userSettings."gitlens.remotes" = [{
+    "type" = "GitLab";
+    "domain" = "code.flowable.com";
+  }];
 }
