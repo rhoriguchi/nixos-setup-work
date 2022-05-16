e658fc857da7f3653f3804d974162b0782496997
Ryan Horiguchi <ryan.horiguchi@mimacom.com>
2022-05-16 21:28:18 +0200

    Remove temp fix

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,16 +1,5 @@
 [
   (self: super: {
-    # TODO remove when merged https://nixpk.gs/pr-tracker.html?pr=172319
-    teams = super.callPackage (import "${
-        super.fetchFromGitHub {
-          owner = "NixOS";
-          repo = "nixpkgs";
-          rev = "b4a61b636a21c7e4608ae9003ccb3865176f395c";
-          sha256 = "sha256-wqb0YsepoPLtFhkCef1U8lErzSoDCApIEELiKV0a3cw=";
-        }
-      }/pkgs/applications/networking/instant-messengers/teams") { };
-  })
-  (self: super: {
     citrix_workspace = super.callPackage ./citrix_workspace { inherit (super) citrix_workspace; };
 
     openconnect-sso = (super.callPackage "${
